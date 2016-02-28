ba2ecec44d163518dc36de15d888b7fb2b872022
lamtanphiho <lamtanphiho1@gmail.com>
2016-02-28 15:43:07 +0700

    ddd

---
+++ server/myapp/shared.js
@@ -0,0 +1,15 @@
+//Client socket
+
+
+var userList = [];
+var memberList 	= [];
+var adminList = [];
+var userCount 	= 0;
+//Game site
+var gamesiteList = [];
+
+exports.userList 	= userList;
+exports.memberList 	= memberList;
+exports.adminList = adminList;
+exports.userCount 	= userCount;
+exports.gamesiteList 	= gamesiteList;
